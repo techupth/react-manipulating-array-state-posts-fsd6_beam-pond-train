ecb3ef6733799bdfac1c5d20600f354944947f6d
Pond <pondwachi.th@gmail.com>
2024-06-06 15:12:36 +0700

    feat: from of pond

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,56 @@
+import { useState } from "react";
+import postData from "./PoatData";
+
 function Posts() {
+  const [posts, setPosts] = useState(postData);
+
+  const handleLike = (like) => {
+    const updateLike = posts.map((post) => {
+      if (post.id === like) {
+        return { ...post, likes: post.likes + 1 };
+      }
+      return post;
+    });
+    setPosts(updateLike);
+  };
+
+  const handleUnLike = (unLike) => {
+    const updateLike = posts.map((post) => {
+      if (post.id === unLike && post.likes > 0) {
+        return { ...post, likes: post.likes - 1 } 
+      } 
+      return post;
+    });
+    setPosts(updateLike);
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {posts.map((post) => (
+          <div key={post.id} class="post-item">
+            <div class="post-header">
+              <h2>{post.title}</h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{post.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">{post.content}</p>
+            <div class="post-actions">
+              <button class="like-button" onClick={() => handleLike(post.id)}>
+                Like
+              </button>
+              <button
+                class="dislike-button"
+                onClick={() => handleUnLike(post.id)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
