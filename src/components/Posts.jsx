54893b04ffb4d790651b2a24068abc135d44bc1b
trainthunder <kitikun.po@gmail.com>
2024-06-06 15:17:17 +0700

    feat: TrainHackhour

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,46 @@
+import { allPost } from "../Data/AllPost";
+import { useState } from "react";
+
 function Posts() {
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {allPost.map((social) => {
+          const [count, setCount] = useState(0);
+          const handleClickPlus = () => {
+            setCount(count + 1);
+          };
+          const handleClickMinus = () => {
+            if (social.likes + count !== 0) {
+              setCount(count - 1);
+            } else {
+              setCount(count - 0);
+            }
+          };
+          return (
+            <div class="post-item">
+              <div class="post-header">
+                <h2>
+                  {social.title} #{social.id}
+                </h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">{social.likes + count}</span>
+                </div>
+              </div>
+              <p class="post-content">{social.content}</p>
+              <div class="post-actions">
+                <button class="like-button" onClick={handleClickPlus}>
+                  Like
+                </button>
+                <button class="dislike-button" onClick={handleClickMinus}>
+                  Dislike
+                </button>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
